587b29b954f401594464aa95fc40eb270fa72cc9
lnovakovi <loranovakovic@gmail.com>
2018-12-13 22:47:44 +0100

    First and second option completed

--- Internship2-Inventory/Program.cs
+++ Internship2-Inventory/Program.cs
@@ -15,7 +15,43 @@ namespace Internship2_Inventory
             Computers = InitializationMethodComputers();
             Phones = InitializationMobiles();
             Vehicles = InitializationMethodVehicles();
+            Console.WriteLine("Izaberi opciju:\n" +
+                              "1) Ispis svih detalja\n" +
+                              "2) Ispis računala kojima ističe garancija\n" +
+                              "3) Ispis koliko komada tehnološke opreme ima baterije\n" +
+                              "4) Ispis svih mobitela\n" +
+                              "5) Ispis računala s odabranim OS\n" +
+                              "6) Ispis svih vozila kojim registracija ističe u idućih mjesec dana");
+            var choice = 0;
+            do
+            {
+               Console.WriteLine("Unesite broj:");
+               choice = int.Parse((Console.ReadLine()));
+            } while (choice < 1 || choice > 6);
 
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter serial number:");
+                    var input = Guid.Parse(Console.ReadLine());
+                    CheckingInvetory(input,Computers,Vehicles,Phones);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter year: ");
+                    var newInput = int.Parse(Console.ReadLine());
+                    PrintComputersWhoseWarrantyExpireInFollowingYear(newInput,Computers);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                    
+                                   
+            }
 
            
 
@@ -24,6 +60,82 @@ namespace Internship2_Inventory
 
         }
 
+        public static void CheckingInvetory(Guid SerialNumber, List<Computer> computerList,List<Vehicles> vehicleList,List<MobilePhones> phoneList)
+        {
+            var br = 0;
+            foreach (var item in computerList)
+            {
+                if (SerialNumber == item.SerialNumber)
+                {
+                    br += 1;
+                    Console.WriteLine("Serial number: " + item.SerialNumber + " OS: " + item.OS + " Transferable: " + item.Transferable + 
+                                      "  Battery Included: " + item.BatteryIncluded + " Date of purchase: " + item.DateOfPurchase +
+                                      "  Description: " + item.Description + "  Months of Warranty: " +
+                                      item.MonthsOfWarranty + "  Purchase price:" + item.PurchasePrice  );
+                }
+            }
+            foreach (var item in vehicleList)
+            {
+                if (SerialNumber == item.SerialNumber)
+                {
+                    br += 1;
+                    Console.WriteLine("Serial number: " + item.SerialNumber + "  Mileage: "+item.Mileage+ "Expiration date of registration: "+ item.ExpirationDateOfRegistration
+                                       + " Date of purchase: " + item.DateOfPurchase +
+                                      "  Description: " + item.Description + "  Months of Warranty: " +
+                                      item.MonthsOfWarranty + "  Purchase price:" + item.PurchasePrice);
+                }
+            }
+            foreach (var item in phoneList)
+            {
+                if (SerialNumber == item.SerialNumber)
+                {
+                    br += 1;
+                    Console.WriteLine("Serial number: " + item.SerialNumber + " Name and surname:"+ item.NameSurname + "Phone number: " +item.PhoneNumber
+                                      + " Date of purchase: " + item.DateOfPurchase +
+                                      "  Description: " + item.Description + "  Months of Warranty: " +
+                                      item.MonthsOfWarranty + "  Purchase price:" + item.PurchasePrice);
+                }
+            }
+
+            if (br == 0)
+            {
+                Console.WriteLine("No item with that serial number");
+            }
+        }
+
+        public static void PrintComputersWhoseWarrantyExpireInFollowingYear(int year,List<Computer> computerList)
+        {
+            var br = 0;
+            foreach (var item in computerList)
+            {
+                var yearsOfWarranty = item.MonthsOfWarranty / 12;
+                var newYear = item.DateOfPurchase.Year + yearsOfWarranty;
+                var monthsLeft = item.MonthsOfWarranty - yearsOfWarranty * 12;
+                var months = item.DateOfPurchase.Month + monthsLeft;
+                if (months > 12)
+                {
+                    var m = months - 12;
+                    newYear += 1;
+                }
+                
+                if (newYear == year)
+                {
+                    br += 1;
+                    Console.WriteLine("Serial number: " + item.SerialNumber + " OS: " + item.OS + " Transferable: " + item.Transferable +
+                                      "  Battery Included: " + item.BatteryIncluded + " Date of purchase: " + item.DateOfPurchase +
+                                      "  Description: " + item.Description + "  Months of Warranty: " +
+                                      item.MonthsOfWarranty + "  Purchase price:" + item.PurchasePrice);
+                }
+
+            }
+
+            if (br == 0)
+            {
+                Console.WriteLine("No computer with warranty that is expiring this year");
+            }
+                
+            }
+        
         public static List<Computer> InitializationMethodComputers()
         {
             var listOfComputers = new List<Computer>();
@@ -32,10 +144,10 @@ namespace Internship2_Inventory
                 "Windows 10 Home\nIntel® Core™ i7 - 6700HQ processor 2.60 GHz 15.6", new DateTime(2015, 10, 5), 12,
                 1321, "Acer");
             var computer2 = new Computer("Linux", false, true,
-                "Linux \nIntel® Core™ i5 - 6720HQ processor 1.60 GHz 15.6", new DateTime(2014, 12, 10), 24,
+                "Linux \nIntel® Core™ i5 - 6720HQ processor 1.60 GHz 15.6", new DateTime(2017, 12, 10), 24,
                 4361, "HP");
             var computer3 = new Computer("Chrome OS", true, true,
-                "Chrome OS Intel® Celeron® N3350 processor Dual-core 1.10 GHz", new DateTime(2013, 12, 17), 24,
+                "Chrome OS Intel® Celeron® N3350 processor Dual-core 1.10 GHz", new DateTime(2017, 12, 17), 24,
                 4361, "HP");
             listOfComputers.Add(computer1);
             listOfComputers.Add(computer2);
