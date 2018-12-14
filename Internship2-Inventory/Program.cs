6e4aa6dbe61ddfd6ed4fbb26c5886ac310bfb8fb
lnovakovi <loranovakovic@gmail.com>
2018-12-14 21:48:49 +0100

    Correction:Final version, added function add/delete and corrected some other details

--- Internship2-Inventory/Program.cs
+++ Internship2-Inventory/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Transactions;
+using System.Xml;
 
 namespace Internship2_Inventory
 {
@@ -13,111 +15,126 @@ namespace Internship2_Inventory
             var Computers = new List<Computer>();
             var Phones = new List<MobilePhones>();
             var Vehicles = new List<Vehicles>();
-            var Technological = new List<TechnologicalEquipment>();
+            
         
             Computers = InitializationMethodComputers();
             Phones = InitializationMobiles();
             Vehicles = InitializationMethodVehicles();
-            foreach (var item in Computers)
-            {
-                Technological.Add(item);
-            }
-            foreach (var item in Phones)
-            {
-                Technological.Add(item);
-            }
-            Console.WriteLine("Choose an option:\n" +
-                              "1) Print all the details of invetory\n" +
-                              "2) Print computers whose warranty expires in year you enter\n" +
-                              "3) Print how many technological equipment has battery included\n" +
-                              "4) Print mobile phones with manufacturer's name you enter\n" +
-                              "5) Print computers with OS you enter\n" +
-                              "6) Print owners of mobile phones whose warranty expires in year you enter\n" +
-                              "7) Print all the vehicles whose registration expires in next month\n" +
-                              "8) Print prices for both categories");
-            
-
-            var enterInput = false;
-            int output;
+          
+            var output = 0;
             do
             {
-                Console.WriteLine("Enter number:");
-                var choiceMade = Console.ReadLine();
-                enterInput = int.TryParse(choiceMade, out output);
-                if (!enterInput )
-                {
-                    Console.WriteLine("Wrong type.");
-                }              
-            } while (!enterInput || output < 1 || output> 8);
+                Console.WriteLine("Choose an option:\n" +
+                             "1) Print all the details of invetory\n" +
+                             "2) Print computers whose warranty expires in year you enter\n" +
+                             "3) Print how many technological equipment has battery included\n" +
+                             "4) Print mobile phones with manufacturer's name you enter\n" +
+                             "5) Print computers with OS you enter\n" +
+                             "6) Print owners of mobile phones whose warranty expires in year you enter\n" +
+                             "7) Print all the vehicles whose registration expires in next month\n" +
+                             "8) Print prices for both categories\n" +
+                             "9) Delete object from inventory\n" +
+                             "10) Add an object to inventory\n" +
+                             "11) Close program");
 
-            switch (output)
-            {
-                case 1:
-                    var success = false;
-                    Guid result;
-                    Console.WriteLine("Enter serial number:");            
-                    do
+
+                var enterInput = false;
+
+                do
+                {
+                    Console.WriteLine("Enter number:");
+                    var choiceMade = Console.ReadLine();
+                    enterInput = int.TryParse(choiceMade, out output);
+                    if (!enterInput)
                     {
-                        var input = Console.ReadLine();
-                        success = Guid.TryParse(input, out result);
-                        if (!success)
+                        Console.WriteLine("Wrong type.");
+                    }
+                } while (!enterInput || output < 1 || output > 10);
+
+                switch (output)
+                {
+                    case 1:
+                        var success = false;
+                        Guid result;
+                        Console.WriteLine("Enter serial number:");
+                        do
                         {
-                            Console.WriteLine("Wrong type.");
-                        }
-                    } while (!success);               
-                    CheckingInvetory(result,Computers,Vehicles,Phones);
-                    break;
-                case 2:
-                    var newInput = "";
-                    var result1 = 0;
-                    Console.WriteLine("Enter year: ");
-                    do
-                    {
-                        newInput = Console.ReadLine();
-                        success = int.TryParse(newInput, out result1);
-                        if (!success)
+                            var input = Console.ReadLine();
+                            success = Guid.TryParse(input, out result);
+                            if (!success)
+                            {
+                                Console.WriteLine("Wrong type.");
+                            }
+                        } while (!success);
+                        CheckingInvetory(result, Computers, Vehicles, Phones);
+                        break;
+                    case 2:
+                        var newInput = "";
+                        var result1 = 0;
+                        Console.WriteLine("Enter year: ");
+                        do
                         {
-                            Console.WriteLine("Wrong type.");
-                        }
-                    } while (!success);                   
-                    PrintComputersWhoseWarrantyExpireInFollowingYear(result1,Computers);
-                    break;
-                case 3:
-                    Console.WriteLine("Number of Tech Equipment with included battery: " + CountTechnologicalEquipmentWithBatteryIncluded(Technological));
-                    break;
-                case 4:
-                    Console.WriteLine("Enter phone's manufacturer: ");
-                    var strInput = Console.ReadLine();
-                    PrintMobilePhonesWithCertainName(strInput,Phones);
-                    break;
-                case 5:
-                    Console.WriteLine("Enter OS: ");
-                    strInput = Console.ReadLine();
-                    PrintComputersWithCertainOS(strInput,Computers);
-                    break;
-                case 6:
-                    Console.WriteLine("Enter year: ");
-                    do
-                    {
-                        newInput = Console.ReadLine();
-                        success = int.TryParse(newInput, out result1);
-                        if (!success)
+                            newInput = Console.ReadLine();
+                            success = int.TryParse(newInput, out result1);
+                            if (!success)
+                            {
+                                Console.WriteLine("Wrong type.");
+                            }
+                        } while (!success);
+                        PrintComputersWhoseWarrantyExpireInFollowingYear(result1, Computers);
+                        break;
+                    case 3:
+                        Console.WriteLine("Number of Tech Equipment with included battery: " + CountTechnologicalEquipmentWithBatteryIncluded(Phones,Computers));
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter phone's manufacturer: ");
+                        var strInput = Console.ReadLine();
+                        PrintMobilePhonesWithCertainName(strInput, Phones);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter OS: ");
+                        strInput = Console.ReadLine();
+                        PrintComputersWithCertainOS(strInput, Computers);
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter year: ");
+                        do
                         {
-                            Console.WriteLine("Wrong type.");
-                        }
-                    } while (!success);
-                    PrintCurrentOwnersWhoseWarrantyExpireInFollowingYear(result1,Phones);
-                    break;
-                case 7:
-                    PrintVehiclesWhoseRegistrationExpiresInNextMonth(Vehicles);
-                    break;
-                case 8:
-                    PrintPrices(Vehicles,Technological);
-                    break;
-                    
-                    
-                                   
-            }
+                            newInput = Console.ReadLine();
+                            success = int.TryParse(newInput, out result1);
+                            if (!success)
+                            {
+                                Console.WriteLine("Wrong type.");
+                            }
+                        } while (!success);
+                        PrintCurrentOwnersWhoseWarrantyExpireInFollowingYear(result1, Phones);
+                        break;
+                    case 7:
+                        PrintVehiclesWhoseRegistrationExpiresInNextMonth(Vehicles);
+                        break;
+                    case 8:
+
+                        PrintPrices(Vehicles, Phones, Computers);
+                        break;
+                    case 9:
+                        Console.WriteLine("Enter from which category you would like to delete item: ");
+                        var answer = Console.ReadLine();
+                        if (answer.ToLower() == "computer") Computers = DelteObject(ref Computers);
+                        else if (answer.ToLower() == "mobile phone") Phones = DeleteObject(ref Phones);
+                        else if (answer.ToLower() == "vehicle") Vehicles = DeleteObject(ref Vehicles);
+                        else Console.WriteLine("Wrong input");
+                        break;
+                    case 10:
+                        Console.WriteLine("Do you want to enter computer,vehicle or mobile phone?");
+                        answer = Console.ReadLine();
+                        if (answer.ToLower() == "computer") Computers=AddObject(ref Computers);
+                        else if (answer.ToLower() == "mobile phone") Phones=AddObject(ref Phones);
+                        else if (answer.ToLower() == "vehicle") Vehicles=AddObject(ref Vehicles);
+                        else Console.WriteLine("Wrong input");
+                        break;
+                }
+            } while (output != 11);
+           
         }
 
         public static void CheckingInvetory(Guid SerialNumber, List<Computer> computerList,List<Vehicles> vehicleList,List<MobilePhones> phoneList)
@@ -196,10 +213,16 @@ namespace Internship2_Inventory
                 
             }
 
-        public static int CountTechnologicalEquipmentWithBatteryIncluded(List<TechnologicalEquipment> techs)
+        public static int CountTechnologicalEquipmentWithBatteryIncluded(List<MobilePhones> mobileList, List<Computer> computerList)
         {
             int br = 0;
-            foreach (var item in techs)
+            foreach (var item in mobileList)
+            {
+                if (item.BatteryIncluded)
+                    br += 1;
+            }
+
+            foreach (var item in computerList)
             {
                 if (item.BatteryIncluded)
                     br += 1;
@@ -300,7 +323,7 @@ namespace Internship2_Inventory
             }
         }
 
-        public static void PrintPrices(List<Vehicles> vehicleList, List<TechnologicalEquipment> techs)
+        public static void PrintPrices(List<Vehicles> vehicleList, List<MobilePhones> mobileList ,List<Computer> computersList)
         {
             var currentPrice = 0m;
             var _20percentOfOriginal = 0m;          
@@ -329,6 +352,17 @@ namespace Internship2_Inventory
                                   "   Purchase price:"  + item.PurchasePrice + 
                                   " Current price: "+ currentPrice + "  Diff: " + (item.PurchasePrice-currentPrice));
             }
+
+            var techs = new List<TechnologicalEquipment>();
+            foreach (var VARIABLE in mobileList)
+            {
+                techs.Add(VARIABLE);
+            }
+
+            foreach (var VARIABLE in computersList)
+            {
+                techs.Add(VARIABLE);
+            }
             var _30percentOfOriginal = 0m;
             var _5percentOfOriginal = 0m;
             var now = DateTime.Now;           
@@ -355,6 +389,131 @@ namespace Internship2_Inventory
                  
             }
         }
+
+        public static List<MobilePhones> AddObject(ref List<MobilePhones> phoneList)
+        {
+            //assumed everything is entered in correct form , no input controls
+            Console.WriteLine("Enter phone number: ");
+            var number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter name an surname of the owner: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Is battery included? Yes/no");
+            var battery = Console.ReadLine();
+            var batteryBool = battery.ToLower() == "yes";
+            Console.WriteLine("Write some description: ");
+            var description = Console.ReadLine();
+            Console.WriteLine("Input date of purchase: (format year-month-day)");
+            var date = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Enter months of warranty: ");
+            var warranty = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter purchase price: ");
+            var price = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number for manufacturer your phone is: 0-SAMSUNG 1-HUAWEI 2-SONY");
+            var seller = int.Parse(Console.ReadLine());
+            var sellerEnum = (Manufacturer)seller;
+            var newObject = new MobilePhones(number,name,batteryBool,description,date,warranty,price,sellerEnum);
+            phoneList.Add(newObject);
+            return phoneList;
+        }
+        public static List<Computer> AddObject(ref List<Computer> computerList)
+        {
+            //assumed everything is entered in correct form , no input controls
+            Console.WriteLine("Enter number for OS: 0-Windows 1-Linux 2-Chrome OS ");
+            var number = int.Parse(Console.ReadLine());
+            var seller = (OperatingSystem) number;
+            Console.WriteLine("Transferable: yes/no ");
+            var transferable = Console.ReadLine();
+            var transferableBool = transferable.ToLower() == "yes";
+            Console.WriteLine("Is battery included? Yes/no");
+            var battery = Console.ReadLine();
+            bool batteryBool = battery.ToLower() == "yes";
+            Console.WriteLine("Write some description: ");
+            var description = Console.ReadLine();
+            Console.WriteLine("Input date of purchase: (format year-month-day)");
+            var date = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Enter months of warranty: ");
+            var warranty = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter purchase price: ");
+            var price = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number for manufacturer your computer is: 6-Acer 7-HP 8-Lenovo");
+            var x = int.Parse(Console.ReadLine());
+            var sellerEnum = (Manufacturer)x;
+            var newObject = new Computer(seller,transferableBool,batteryBool,description,date,warranty,price,sellerEnum);
+            computerList.Add(newObject);
+            return computerList;
+
+        }
+        public static List<Vehicles> AddObject(ref List<Vehicles> vehicleList)
+            {
+                //assumed everything is entered in correct form , no input controls
+                Console.WriteLine("Enter expiration date: format year-month-day ");
+                var expirationDate = Convert.ToDateTime(Console.ReadLine());
+                Console.WriteLine("Mileage: ");
+                var mileage = double.Parse(Console.ReadLine());
+                Console.WriteLine("Write some description: ");
+                var description = Console.ReadLine();
+                Console.WriteLine("Input date of purchase: (format year-month-day)");
+                var date = Convert.ToDateTime(Console.ReadLine());
+                Console.WriteLine("Enter months of warranty: ");
+                var warranty = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter purchase price: ");
+                var price = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Enter number for manufacturer your vehicles is: 3-BMW 4-Opel 5-Ford");
+                var x = int.Parse(Console.ReadLine());
+                var sellerEnum = (Manufacturer)x;
+                var newObject= new Vehicles(expirationDate,mileage,description,date,warranty,price,sellerEnum);
+                vehicleList.Add(newObject);
+                return vehicleList;
+
+            }
+
+        public static List<MobilePhones> DeleteObject(ref List<MobilePhones> mobileList)
+        {
+            Console.WriteLine("Write number of object that you want to delete: ");
+            var choice = 0;  
+            for (int i = 0; i < mobileList.Count; i++)
+                {
+                    Console.WriteLine(i + 1 + " Serial number: " + mobileList[i].SerialNumber + " Manufacturer: " +
+                                      mobileList[i].NameOfManufacturer);
+                }
+
+             
+            choice = int.Parse(Console.ReadLine());
+            mobileList[choice - 1] = null;
+            mobileList.RemoveAt(choice - 1);
+            return mobileList;
+            
+        }
+
+        public static List<Computer> DelteObject(ref List<Computer> computerList)
+        {
+            Console.WriteLine("Write number of object that you want to delete: ");
+            var choice = 0;
+            for (int i = 0; i < computerList.Count; i++)
+            {
+                Console.WriteLine(i + 1 + " Serial number: " + computerList[i].SerialNumber + " Manufacturer: " + computerList[i].NameOfManufacturer);
+                
+            }
+            choice = int.Parse(Console.ReadLine());
+            computerList[choice - 1] = null;
+            computerList.RemoveAt(choice - 1);
+            return computerList;
+        }
+
+        public static List<Vehicles> DeleteObject(ref List<Vehicles> vehicle)
+        {
+            Console.WriteLine("Write number of object that you want to delete: ");
+            var choice = 0;
+            for (int i = 0; i < vehicle.Count; i++)
+            {
+                Console.WriteLine(i + 1 + " Serial number: " + vehicle[i].SerialNumber + " Manufacturer: " + vehicle[i].NameOfManufacturer);
+            }
+
+            choice = int.Parse(Console.ReadLine());
+            vehicle[choice - 1] = null;
+            vehicle.RemoveAt(choice-1);
+            return vehicle;
+        }
         public static List<Computer> InitializationMethodComputers()
         {
             var listOfComputers = new List<Computer>();
@@ -377,7 +536,6 @@ namespace Internship2_Inventory
             return listOfComputers;
 
         }
-       
         public static List<MobilePhones> InitializationMobiles()
         {
             var listOfMobiles = new List<MobilePhones>();
